7d695a06c78a6399085bd7efd87557205e3a1612
Austin Major <mr.austinmajor@gmail.com>
2019-08-21 23:55:52 -0500

    adding class directory, adding source for insertion sort

---
+++ class/insertion-sort.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+void insertionSort(int [], int);
+void printArray(int[], int);
+void sizeofArray(int [], int);
+
+void insertionSort(int arr[], int n)
+{
+  int i, j;
+  for(i = 1; i < n; i++)
+  {
+    for(j = i; j > 0 && arr[j] < arr[j-1]; j--)
+    {
+      swap(arr[j], arr[j-1]);
+      printArray(arr, n);
+    }
+  }
+}
+
+void printArray(int arr[], int n)
+{
+  int i;
+  for(i = 0; i < n; i++)
+  {
+    cout << arr[i] << " ";
+  }
+  
+  cout << endl;
+}
+
+int main() {
+  int arr[] = {8,3,5,1,2};
+  int n = sizeof(arr) / sizeof(arr[0]);
+
+  insertionSort(arr, n);
+
+  return 0;
+}
