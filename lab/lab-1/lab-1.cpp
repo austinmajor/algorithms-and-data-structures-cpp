3daff4193079a8b47431be30342da153b64b1b24
Austin Major <mr.austinmajor@gmail.com>
2019-08-25 17:06:21 -0500

    adding some of the source for lab-1.cpp

--- lab/lab-1/lab-1.cpp
+++ lab/lab-1/lab-1.cpp
@@ -2,7 +2,75 @@
 
 using namespace std;
 
+class DynamicArray{
+public:
+  // Member variables
+  // these need to be private
+  int * arr;
+  int size;
+
+  // Default Constructor
+  DynamicArray()
+  {
+    arr = new int[10];
+  }
+
+  // Paramterized Constructor
+  DynamicArray(int size)
+  {
+    arr = new int[size];
+  }
+  
+  // Copy Constructor
+  DynamicArray(int * d)
+  {
+    *arr = *d;
+  }
+
+  // Copy Assignment Operator
+  DynamicArray(const DynamicArray&)
+
+  // Destructor
+  ~DynamicArray()
+  {
+    cout << "Destructor called" << endl;
+  }
+
+  // Member Functions
+  void fill()
+  {
+    int i;
+    for(i = 0; i < 10; i++)
+    {
+      arr[i] = rand();
+    }
+  }
+
+  void sort()
+  {
+    int i, j;
+    for(i = 1; i < n; i++)
+    {
+      for(j = i; j > 0 && arr[j] < arr[j-1]; j--)
+      {
+        swap(arr[j], arr[j-1]);
+        printArray(arr, n);
+      }
+    }
+  }
+
+  void print(int size)
+  {
+    int i;
+    for(i = 0; i < size; i++)
+    {
+      cout << arr[i] << " ";
+    }
+  }
+};
+
 int main()
 {
 
+  return 0;
 }
