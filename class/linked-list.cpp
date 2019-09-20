39960a2bd1db4931841d27474327d2325a887bb7
Austin Major <mr.austinmajor@gmail.com>
2019-09-19 22:44:23 -0500

    adding new lab and hw

--- class/linked-list.cpp
+++ class/linked-list.cpp
@@ -1,8 +1,8 @@
+// 20190909_linked_list.cpp : This file contains the 'main' function. Program execution begins and ends there.
 #include <iostream>
 
-using namespace std;
-
 // define Node
+
 typedef int dataType;
 
 struct Node
@@ -13,57 +13,131 @@ struct Node
     Node(dataType value) : data(value), next(nullptr) {}
 };
 
+// for Node* pointer type
+typedef Node *NodePtr;
+
+// function prototype
+void traverse(Node *head);
+void add_head_1(NodePtr &head, dataType num);
+
+// add_head_1_5, which uses reference and pointer;
+void add_head_1_5(Node *&head, dataType num);
+
+// add_head_2(), which uses double pointer
+void add_head_2(Node **head, dataType num);
+
+// add at tail
+void add_tail(NodePtr &head, dataType num);
+
+// TO_DO: search a number
+
+// delete function
+void del(NodePtr &head, dataType num);
+
+// big Oh analysis
+
+// doubly linked list
+
+int main()
+{
+
+    NodePtr head = nullptr;
+
+    add_head_1(head, 10);
+    add_head_1(head, 20);
+
+    add_head_1_5(head, 30);
+
+    add_head_2(&head, 40);
+
+    traverse(head);
+}
+
 void traverse(Node *head)
 {
     for (Node *tempPtr = head; tempPtr != nullptr; tempPtr = tempPtr->next)
     {
-        cout << tempPtr->data << endl;
+        std::cout << tempPtr->data << "\n";
     }
 }
-
-// define NodePtr
-typedef Node *NodePtr;
 
 void add_head_1(NodePtr &head, dataType num)
 {
     Node *temp = new Node(num);
-
     temp->next = head;
-
     head = temp;
 }
 
-int main()
+void add_head_1_5(Node *&head, dataType num)
 {
-    // Node n1(11);
-    // Node n2(22);
-    // Node n3(33);
-    // Node n4(44);
+    Node *temp = new Node(num);
+    temp->next = head;
+    head = temp;
+}
 
-    // n1.next = &n2;
-    // n2.next = &n3;
-    // n3.next = &n4;
+void add_head_2(Node **head, dataType num)
+{
+    Node *temp = new Node(num);
+    temp->next = *head;
+    *head = temp;
+}
 
-    // option 1
-    // Node* head = &n1;
-    // traverse(head);
+void add_tail(NodePtr &head, dataType num)
+{
+    if (head == nullptr)
+    {
+        add_head_1(head, num);
+        return;
+    }
+    NodePtr curr = head;
 
-    // option 2
-    // int a = 10;
-    // int* pa = &a;
-    // int** ppa = &pa;
+    // this loop will point curr to the last node.
+    while (curr->next != nullptr)
+    {
+        curr = curr->next;
+    }
 
-    // cout << a << "\t" << &a << endl;
-    // cout << pa << "\t" << &pa << endl;
-    // cout << ppa << "\t" << &ppa << endl;
-    // cout << *ppa << "\t" << **ppa << endl;
+    // the curr should be pointing at the last node
+    Node *temp = new Node(num);
+    curr->next = temp;
+}
 
-    // option 3
-    NodePtr head = nullptr;
-    add_head_1(head, 10);
-    add_head_1(head, 20);
-    add_head_1(head, 30);
-    traverse(head);
+void del(NodePtr &head, dataType num)
+{
+    // case 1: the l.l. is empty
+    if (head == nullptr)
+    {
+        return;
+    }
 
-    return 0;
+    // case 2: the head node matches the target num
+    if (head->data == num)
+    {
+        NodePtr temp = head;
+        head = head->next;
+        delete temp;
+        return;
+    }
+
+    //case 3 & 4
+    NodePtr prev = nullptr;
+    NodePtr curr = head;
+    while (curr != nullptr && curr->data != num)
+    {
+        prev = curr;
+        curr = curr->next;
+    }
+
+    // after the loop, either we exhaust the linked list with no matching,
+    // or we have found the match.
+
+    // case 3: no match found in linked list
+    if (curr == nullptr)
+    {
+        return;
+    }
+
+    // case 4: curr is pointing at the target num
+    prev->next = curr->next;
+    delete curr;
 }
